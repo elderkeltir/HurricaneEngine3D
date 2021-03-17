42e1ca78a0d7b6cda3a194aef4e981da7e529e78
rustyserg <rusty_serg@i.ua>
2021-03-17 19:23:38 +0200

    commit after review

--- include/common/logger.h
+++ include/common/logger.h
@@ -1,46 +1,57 @@
 #pragma once
+
 #include <string>
 #include <fstream>
 #include <ctime>
 #include <chrono> 
+#include <assert.h>
 
 class logger {
 public:
-	enum loglevel
+	enum log_level
 	{
-		logCRITICAL, logERROR, logWARNING, logDEBUG, logINFO,
+		ll_CRITICAL, 
+		ll_ERROR, 
+		ll_WARNING, 
+		ll_DEBUG, 
+		ll_INFO
 	};
-	Logger(std::string file_name, loglevel level) : full_buffer_size (100)
+	logger(const std::string &file_name, log_level level) : m_full_buffer_size (100), m_log_level_needed (level)
 	{
-		log_level_needed = level;
-		m_fileStream.open(file_name, std::ofstream::m_fileStream | std::ofstream::trunc);
+		m_fileStream.open(file_name, std::ofstream::out | std::ofstream::trunc);
+		assert(m_fileStream.is_open());
+	
 	}
-	~Logger()
+	~logger()
 	{
-		m_fileStream << m_full_buffer;
+		if (m_buffer.length() >= 1)
+		{
+			m_fileStream << m_buffer;
+		}
 		m_fileStream.close();
 	}
-	const char* timenow()
+	const std::string timenow()
 	{
 		auto timenow = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-		return  ctime(&timenow);
+		std::string time_now = ctime(&timenow);
+		return  time_now;
 	}
 
 	template<typename... Ts>
-	void hlog(int log_level, std::string word, Ts ... args)
+	void hlog(int log_level, std::string line, Ts ... args)
 	{
-		if (log_level <= log_level_needed)
+		if (log_level <= m_log_level_needed)
 		{
 			const int buffer_size = 100;
 			char buffer[buffer_size];
-			snprintf(buffer, buffer_size, word.c_str(), args...);
-			std::string wordM = buffer;
-			wordM = timenow() + wordM + "\n";
-			m_full_buffer = m_full_buffer + wordM;
-			if (m_full_buffer.length() >= m_full_buffer_size)
+			snprintf(buffer, buffer_size, line.c_str(), args...);
+			std::string text_line = buffer;
+			text_line = timenow() + text_line + "\n";
+			m_buffer = m_buffer + text_line;
+			if (m_buffer.length() >= m_full_buffer_size)
 			{
-				m_fileStream << m_full_buffer;
-				m_full_buffer.clear();
+				m_fileStream << m_buffer;
+				m_buffer.clear();
 			}
 		}
 		else
@@ -51,8 +62,8 @@ public:
 
 private:
 	std::ofstream m_fileStream;
-	std::string m_full_buffer;
-	const int m_full_buffer_size;
-	loglevel log_level_needed;
+	std::string m_buffer;
+	const size_t m_full_buffer_size;
+	log_level m_log_level_needed;
 };
 
