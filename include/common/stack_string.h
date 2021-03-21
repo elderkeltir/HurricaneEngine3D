5c736427ceb568156f05bb9e24ae8eb246155698
Gomer0513 <Gomer0513@gmail.com>
2021-03-21 15:45:06 +0200

    adjustments after review #2

--- include/common/stack_string.h
+++ include/common/stack_string.h
@@ -6,49 +6,47 @@
 
 #pragma once
 
-#ifndef MAX_CHARACTER
-#define MAX_CHARACTER 256
-#endif
-
+template<size_t size>
 class stack_string {
 public:
     const char* c_str() {
         return m_buffer;
     }
     
-    /*
-         Constructors
-    */
-    
     stack_string() {
         m_buffer[0] = '\0';
     }
     
-    // Create
-    stack_string(char* cstring) {
-        if (strlen(cstring) <= MAX_CHARACTER) {
+    explicit stack_string(const char* cstring) {
+        assert(cstring);
+        if (strlen(cstring) <= size) {
             strcpy(m_buffer, cstring);
+        } else {
+            // Reached the buffer's size, clean buffer
+            m_buffer[0] = '\0';
         }
     }
     
-    // Copy object
     stack_string(const stack_string &stackString) {
-        strcpy(m_buffer, stackString.m_buffer);
+        stackString(stackString.m_buffer);
     }
     
-    // Swap two objects
     stack_string(stack_string &&other) {
         std::swap(m_buffer, other.m_buffer);
     }
     
-    // Destructor
     ~stack_string() {
         m_buffer[0] = '\0';
     }
     
-    /*
-         Operators
-    */
+    const stack_string &operator=(const char* cstring) {
+        assert(is_valid());
+        if (cstring != this->m_buffer) {
+            strcpy(m_buffer, cstring);
+        }
+        
+        return *this;
+    }
     
     const stack_string &operator=(stack_string const& other) {
         if (&other != this) {
@@ -67,78 +65,114 @@ public:
     }
     
     const stack_string &operator+(stack_string const& cstring) {
+        return operator+(cstring.m_buffer);
+    }
+    
+    const stack_string &operator+(const char* cstring) {
+        assert(is_valid());
         const size_t current_size_array = strlen(m_buffer);
-        const size_t appended_size_array = strlen(cstring.m_buffer);
+        const size_t appended_size_array = strlen(cstring);
         
-        if (appended_size_array + current_size_array + 1 > MAX_CHARACTER) {
+        if (appended_size_array + current_size_array + 1 > size) {
             assert(false);
             return *this;
         }
-        strcat(m_buffer, cstring.m_buffer);
+        strcat(m_buffer, cstring);
         return *this;
     }
     
-    /*
-        Methods
-     */
-    
-    size_t find_index(const stack_string &stackString) {
-        if (strlen(stackString.m_buffer) == 0) {
+    size_t find_index(const char* cstring) const {
+        assert(is_valid());
+        if ((cstring) && (cstring[0] == '\0')) {
             return -1;
         }
         
-        const char* first_occurence = strstr(m_buffer, stackString.m_buffer);
+        const char* first_occurence = strstr(m_buffer, cstring);
         
-        if (first_occurence != NULL) {
+        if (first_occurence) {
             return first_occurence-m_buffer;
         }
         
         return -1;
     }
     
-    void append(const stack_string &stackString) {
+    size_t find_index(const stack_string &stackString) const {
+        return find_index(stackString.m_buffer);
+    }
+    
+    void append(const char* cstring) {
+        assert(is_valid());
         const size_t current_size_array = strlen(m_buffer);
-        const size_t appended_size_array = strlen(stackString.m_buffer);
+        const size_t appended_size_array = strlen(cstring);
         
-        if (appended_size_array + current_size_array + 1 > MAX_CHARACTER) {
+        if (appended_size_array + current_size_array + 1 > size) {
             assert(false);
             return;
         }
         
-        strcpy(m_buffer+current_size_array, stackString.m_buffer);
+        strcpy(m_buffer+current_size_array, cstring);
     }
     
-    void insert(const size_t pos, const stack_string &stackString) {
+    void append(const stack_string &stackString) {
+        append(stackString.m_buffer);
+    }
+    
+    void insert(const size_t pos, const char* cstring) {
+        assert(is_valid());
         const size_t current_size_string = strlen(m_buffer);
-        const size_t appended_size_array = strlen(stackString.m_buffer);
+        const size_t appended_size_array = strlen(cstring);
         
-        char temp[MAX_CHARACTER];
+        char temp[size];
         
-        if (current_size_string + appended_size_array + 1 > MAX_CHARACTER) {
+        if (current_size_string + appended_size_array + 1 > size) {
             assert(false);
             return;
         }
 
         strcpy(temp, m_buffer+pos);
-        strcpy(m_buffer+pos, stackString.m_buffer);
+        strcpy(m_buffer+pos, cstring);
         strcpy(m_buffer+pos+appended_size_array, temp);
+    }
+    
+    void insert(const size_t pos, const stack_string &stackString) {
+        insert(pos, stackString.m_buffer);
+    }
+    
+    void replace(const size_t from, const char* cstring) {
+        assert(is_valid());
+        strcpy(m_buffer+from, cstring);
     }
     
     void replace(const size_t from, const stack_string &stackString) {
         strcpy(m_buffer+from, stackString.m_buffer);
     }
     
-    bool empty() {
-        if (strlen(m_buffer) == 0) {
-            return true;
+    void erase(const size_t pos, const size_t length) {
+        if (pos == 0 && length == size) {
+            // Delete whole buffer
+            m_buffer[pos] = '\0';
+        } else {
+            char temp[size];
+            
+            strcpy(temp, m_buffer+pos+length);
+            strcpy(m_buffer+pos, temp);
         }
-        return  false;
     }
     
-    size_t lenght() {
+    bool is_valid() const {
+        return m_buffer && strlen(m_buffer) < size;
+    }
+    
+    bool empty() const {
+        assert(is_valid());
+        return m_buffer && m_buffer[0] == '\0';
+    }
+    
+    size_t lenght() const {
+        assert(is_valid());
         return strlen(m_buffer);
     }
     
 private:
-    char m_buffer[MAX_CHARACTER];
+    char m_buffer[size];
 };
