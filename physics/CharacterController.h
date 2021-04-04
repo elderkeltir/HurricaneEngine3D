55e851910309aac810913657aeb767b685473569
Esentiel <a.bursuk@gmail.com>
2021-04-04 13:29:23 +0300

    fixed thread sync errors

--- physics/CharacterController.h
+++ physics/CharacterController.h
@@ -4,17 +4,19 @@
 
 namespace physx {
 	class PxController;
+    class PxScene;
 }
 
 
 class CharacterController{
 public:
-    void Initialize(physx::PxController* cct);
+    void Initialize(physx::PxController* cct, physx::PxScene * scene);
     void Move(const physx::PxVec3 &disp, float dt, bool jump);
     physx::PxVec3 GetPosition() const;
 
 private:
     physx::PxController * m_cct;
+    physx::PxScene * r_scene;
     physx::PxVec3 m_lastPos;
     physx::PxVec3 m_velocity;
 };
